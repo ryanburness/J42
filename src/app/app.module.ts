0b8b45a200b56026bb98e05cea054629531dc5b2
ryanburness <ryanburness94@gmail.com>
2017-03-21 19:12:38 +0000

    added album, carousel and jumbotron components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,20 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
-import { AppRoutingModule } from "app/app-routing/app-routing.module";
-import { PlaysComponent } from './plays/plays/plays.component';
+import { AppRoutingModule } from 'app/app-routing/app-routing.module';
+import { PlaysComponent } from './plays/plays.component';
+import { JumbotronComponent } from './shared/jumbotron/jumbotron.component';
+import { CarouselComponent } from './shared/carousel/carousel.component';
+import { AlbumComponent } from './shared/album/album.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    PlaysComponent    
+    PlaysComponent,
+    JumbotronComponent,
+    CarouselComponent,
+    AlbumComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/welcome/welcome.component.ts
+++ src/app/home/welcome/welcome.component.ts
@@ -5,6 +5,5 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent {
-
+export class WelcomeComponent {  
 }
